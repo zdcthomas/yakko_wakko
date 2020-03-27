02761628eece948a7a3ced84db7fa3cbfc182794
zdcthomas <zdcthomas@yahoo.com>
2020-03-26 23:20:36 -0600

    adds multi line fish prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -9,27 +9,30 @@ set __fish_git_prompt_char_upstream_ahead '⤵'
 set __fish_git_prompt_char_upstream_ahead '⤴'
 
 function fish_prompt
+  set -l last_status $status
   set -g fish_prompt_pwd_dir_length 0
   set_color normal
-  
-  set last_status $status
-  
-  if not set -q __git_cb
-    set __git_cb ":"(set_color green)(git branch ^/dev/null | grep \* | sed 's/* //')(set_color normal)""
-  end
-
-
   echo -n (prompt_pwd)
-  #echo -n $__git_cb
   echo -n (__fish_git_prompt)
-  fishy
+  printf '\n| '
+  status_fishy $last_status
 end
 
-function fishy
-  set_color blue 
-  echo -n ' >'
-  set_color red
-  echo -n '<'
-  set_color green
-  echo -n '>'
+function status_fishy
+  if [ $argv[1] -ne 0 ]
+    set_color red 
+    echo -n '>'
+    echo -n '<'
+    set_color normal
+    echo -n $argv[1]
+    set_color red
+    echo -n '>'
+  else
+    set_color cyan 
+    echo -n '>'
+    set_color red
+    echo -n '<'
+    set_color brgreen
+    echo -n '> '
+  end
 end
