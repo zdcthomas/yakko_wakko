9de3ef0798484cf9d686d95c8199ad4fb4f4ef47
Zachary Thomas <zdcthomas@yahoo.com>
2021-08-27 10:37:32 -0600

    it was just the autopairs autoselect!
    That really shouldn't be configured there, but hey, what are you gonna
    do? Honestly, a lot of the nvim lua configuration options aren't super
    idiomatic. Oh well, as the community matures that'll get better and
    settle down.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -82,7 +82,6 @@ return require('packer').startup({
         require("nvim-autopairs.completion.cmp").setup({
           map_cr = true, --  map <CR> on insert mode
           map_complete = true, -- it will auto insert `(` after select function or method item
-          auto_select = true -- automatically select the first item
         })
       end
     }
