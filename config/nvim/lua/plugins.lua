0d0f995e49930b698de66a5e79156c4fe0fd843e
Zachary Thomas <zdcthomas@yahoo.com>
2022-05-22 21:41:37 -0600

    update neorg gtd config and plugins.lua

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -3,14 +3,14 @@ local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 local packer_bootstrap
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
+	packer_bootstrap = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
 end
 
 -- potential plugins
@@ -21,299 +21,301 @@ end
 -- * rbong/vim-buffest
 
 function PluginIsLoaded(plugin_name)
-  return packer_plugins[plugin_name] and packer_plugins[plugin_name].loaded
+	return packer_plugins[plugin_name] and packer_plugins[plugin_name].loaded
 end
 
 return require("packer").startup({
-  function(use)
-    use("lewis6991/impatient.nvim")
-    use("christoomey/vim-sort-motion")
-    use("michaeljsmith/vim-indent-object")
-    use("wbthomason/packer.nvim")
-    use("MunifTanjim/nui.nvim")
-    use({ "kevinhwang91/nvim-bqf", ft = "qf" })
-    use({ "seandewar/nvimesweeper", cmd = { "Nvimesweeper" } })
-    use({ "tpope/vim-commentary", keys = { "gc" }, cmd = { "Commentary" } })
-    use({
-      "b0o/incline.nvim",
-      config = function()
-        require("incline").setup({
-          highlight = {
-            groups = {
-              InclineNormal = "PmenuSel",
-              InclineNormalNC = "PmenuSbar",
-            },
-          },
-          hide = {
-            focused_win = false,
-          },
-        })
-      end,
-    })
+	function(use)
+		use("lewis6991/impatient.nvim")
+		use("christoomey/vim-sort-motion")
+		use("michaeljsmith/vim-indent-object")
+		use("wbthomason/packer.nvim")
+		use("MunifTanjim/nui.nvim")
 
-    use({
-      "stevearc/dressing.nvim",
-      config = config.dressing,
-    })
+		-- Find and replace goodness
+		use({
+			"windwp/nvim-spectre",
+			requires = {
+				"nvim-lua/plenary.nvim",
+			},
+		})
+		use({ "kevinhwang91/nvim-bqf", ft = "qf" })
+		use({ "seandewar/nvimesweeper", cmd = { "Nvimesweeper" } })
+		use({ "tpope/vim-commentary", keys = { "gc" }, cmd = { "Commentary" } })
+		use({
+			"b0o/incline.nvim",
+			config = function()
+				require("incline").setup({
+					highlight = {
+						groups = {
+							InclineNormal = "PmenuSel",
+							InclineNormalNC = "PmenuSbar",
+						},
+					},
+					hide = {
+						focused_win = false,
+					},
+				})
+			end,
+		})
 
-    use({
-      "takac/vim-hardtime",
-      config = config.hardtime,
-    })
+		use({
+			"stevearc/dressing.nvim",
+			config = config.dressing,
+		})
 
-    use({
-      "folke/zen-mode.nvim",
-      config = function()
-        require("zen-mode").setup({
-          window = {
-            width = 135,
-            backdrop = 1,
-          },
-          -- your configuration comes here
-          -- or leave it empty to use the default settings
-          -- refer to the configuration section below
-        })
-      end,
-    })
+		use({
+			"takac/vim-hardtime",
+			config = config.hardtime,
+		})
 
-    use({
-      "jbyuki/venn.nvim",
-      keys = { "<Leader>v" },
-      config = config.venn,
-    })
+		use({
+			"folke/zen-mode.nvim",
+			config = function()
+				require("zen-mode").setup({
+					window = {
+						width = 135,
+						backdrop = 1,
+					},
+				})
+			end,
+		})
 
-    use({
-      "rcarriga/nvim-notify",
-      config = config.notify,
-    })
+		use({
+			"jbyuki/venn.nvim",
+			keys = { "<Leader>v" },
+			config = config.venn,
+		})
 
-    use({
-      "nicwest/vim-camelsnek",
-      config = config.camelsnek,
-    })
+		use({
+			"rcarriga/nvim-notify",
+			config = config.notify,
+		})
 
-    use({
-      "dhruvasagar/vim-table-mode",
-      cmd = { "TableModeToggle" },
-    })
+		use({
+			"nicwest/vim-camelsnek",
+			config = config.camelsnek,
+		})
 
-    use({
-      "Julian/vim-textobj-variable-segment",
-      requires = {
-        "kana/vim-textobj-user",
-      },
-    })
+		use({
+			"dhruvasagar/vim-table-mode",
+			cmd = { "TableModeToggle" },
+		})
 
-    use({
-      "rebelot/kanagawa.nvim",
-      config = config.kanagawa,
-    })
+		use({
+			"Julian/vim-textobj-variable-segment",
+			requires = {
+				"kana/vim-textobj-user",
+			},
+		})
 
-    use({
-      "rose-pine/neovim",
-      as = "rose-pine",
-      config = config.rose_pine,
-    })
+		use({
+			"rebelot/kanagawa.nvim",
+			config = config.kanagawa,
+		})
 
-    use({
-      "https://gitlab.com/yorickpeterse/nvim-pqf.git",
-      config = function()
-        require("pqf").setup()
-      end,
-    })
+		use({
+			"rose-pine/neovim",
+			as = "rose-pine",
+			config = config.rose_pine,
+		})
 
-    use({
-      "machakann/vim-sandwich",
-      config = function()
-        vim.cmd([[
+		use({
+			"https://gitlab.com/yorickpeterse/nvim-pqf.git",
+			config = function()
+				require("pqf").setup()
+			end,
+		})
+
+		use({
+			"machakann/vim-sandwich",
+			config = function()
+				vim.cmd([[
           exec 'source ' . "~/.vim/settings/plugins/sandwich_settings.vim"
         ]])
-      end,
-    })
+			end,
+		})
 
-    use({
-      "windwp/nvim-autopairs",
-      on = "InsertEnter",
-      config = config.autopairs,
-    })
+		use({
+			"windwp/nvim-autopairs",
+			on = "InsertEnter",
+			config = config.autopairs,
+		})
 
-    use({
-      "hrsh7th/nvim-cmp",
-      requires = {
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-calc",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-vsnip",
-        "onsails/lspkind-nvim",
-        "petertriho/cmp-git",
-        {
-          "hrsh7th/vim-vsnip",
-          requires = { "hrsh7th/vim-vsnip-integ" },
-          config = function()
-            vim.g.vsnip_snippet_dir = "~/yakko_wakko/config/nvim/snippets"
-          end,
-        },
-      },
-      config = function()
-        -- See lspconfig comment on why this is in a function wrapper
-        require("config.cmp").setup()
-      end,
-    })
+		use({
+			"hrsh7th/nvim-cmp",
+			requires = {
+				"hrsh7th/cmp-buffer",
+				"hrsh7th/cmp-calc",
+				"hrsh7th/cmp-cmdline",
+				"hrsh7th/cmp-nvim-lsp",
+				"hrsh7th/cmp-path",
+				"hrsh7th/cmp-vsnip",
+				"onsails/lspkind-nvim",
+				"petertriho/cmp-git",
+				{
+					"hrsh7th/vim-vsnip",
+					requires = { "hrsh7th/vim-vsnip-integ" },
+					config = function()
+						vim.g.vsnip_snippet_dir = "~/yakko_wakko/config/nvim/snippets"
+					end,
+				},
+			},
+			config = function()
+				-- See lspconfig comment on why this is in a function wrapper
+				require("config.cmp").setup()
+			end,
+		})
 
-    use({
-      "kyazdani42/nvim-web-devicons",
-      config = function()
-        require("nvim-web-devicons").setup({
-          default = true,
-        })
-      end,
-    })
+		use({
+			"kyazdani42/nvim-web-devicons",
+			config = function()
+				require("nvim-web-devicons").setup({
+					default = true,
+				})
+			end,
+		})
 
-    use({
-      "nvim-lualine/lualine.nvim",
-      requires = {
-        { "nvim-lua/lsp-status.nvim" },
-      },
-      config = function()
-        require("config.lualine").setup()
-      end,
-    })
+		use({
+			"nvim-lualine/lualine.nvim",
+			requires = {
+				{ "nvim-lua/lsp-status.nvim" },
+			},
+			config = function()
+				require("config.lualine").setup()
+			end,
+		})
 
-    use({
-      "lewis6991/gitsigns.nvim",
-      requires = {
-        "nvim-lua/plenary.nvim",
-      },
-      config = config.git_signs,
-    })
+		use({
+			"lewis6991/gitsigns.nvim",
+			requires = {
+				"nvim-lua/plenary.nvim",
+			},
+			config = config.git_signs,
+		})
 
-    use({
-      "junegunn/vim-easy-align",
-      keys = {
-        { "x", "ga" },
-        { "n", "ga" },
-      },
-      cmd = "EasyAlign",
-      config = config.easy_align,
-    })
+		use({
+			"junegunn/vim-easy-align",
+			keys = {
+				{ "x", "ga" },
+				{ "n", "ga" },
+			},
+			cmd = "EasyAlign",
+			config = config.easy_align,
+		})
 
-    use({
-      "mhinz/vim-startify",
-      branch = "center",
-      config = function()
-        require("config.startify").setup()
-      end,
-    })
-    use({
-      "tommcdo/vim-ninja-feet",
-    })
+		use({
+			"mhinz/vim-startify",
+			branch = "center",
+			config = function()
+				require("config.startify").setup()
+			end,
+		})
 
-    use({
-      "nvim-telescope/telescope.nvim",
-      requires = {
-        { "nvim-lua/popup.nvim" },
-        { "nvim-lua/plenary.nvim" },
-        {
-          "kyazdani42/nvim-web-devicons",
-          config = function()
-            require("nvim-web-devicons").setup({
-              default = true,
-            })
-          end,
-        },
-        { "nvim-telescope/telescope-ui-select.nvim" },
-        { "nvim-telescope/telescope-file-browser.nvim" },
-        { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
-      },
-      config = function()
-        require("config.telescope").setup()
-      end,
-    })
+		use({
+			"nvim-telescope/telescope.nvim",
+			requires = {
+				{ "nvim-lua/popup.nvim" },
+				{ "nvim-lua/plenary.nvim" },
+				{
+					"kyazdani42/nvim-web-devicons",
+					config = function()
+						require("nvim-web-devicons").setup({
+							default = true,
+						})
+					end,
+				},
+				{ "nvim-telescope/telescope-ui-select.nvim" },
+				{ "nvim-telescope/telescope-file-browser.nvim" },
+				{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+			},
+			config = function()
+				require("config.telescope").setup()
+			end,
+		})
 
-    use({
-      "nvim-treesitter/nvim-treesitter",
-      run = ":TSUpdate",
-      requires = {
-        "p00f/nvim-ts-rainbow",
-        {
-          "nvim-treesitter/playground",
-          cmd = "TSPlaygroundToggle",
-        },
-      },
-      config = function()
-        require("config.treesitter").setup()
-      end,
-    })
+		use({
+			"nvim-treesitter/nvim-treesitter",
+			run = ":TSUpdate",
+			requires = {
+				"p00f/nvim-ts-rainbow",
+				{
+					"nvim-treesitter/playground",
+					cmd = "TSPlaygroundToggle",
+				},
+			},
+			config = function()
+				require("config.treesitter").setup()
+			end,
+		})
 
-    use({
-      "neovim/nvim-lspconfig",
-      requires = {
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/vim-vsnip",
-        "kosayoda/nvim-lightbulb",
-        "nvim-lua/lsp-status.nvim",
-        "nvim-telescope/telescope.nvim",
-        "ray-x/lsp_signature.nvim",
-        "williamboman/nvim-lsp-installer",
-      },
-      config = function()
-        -- Ok I really don't understand this. If I remove this function
-        -- wrapper, then any local function defined within config/lspconfig
-        -- won't be usable by the setup function. This makes no sense.
-        require("config.lspconfig").setup()
-      end,
-    })
+		use({
+			"neovim/nvim-lspconfig",
+			requires = {
+				"ckipp01/stylua-nvim",
+				"hrsh7th/nvim-cmp",
+				"hrsh7th/vim-vsnip",
+				"kosayoda/nvim-lightbulb",
+				"nvim-lua/lsp-status.nvim",
+				"nvim-telescope/telescope.nvim",
+				"ray-x/lsp_signature.nvim",
+				"williamboman/nvim-lsp-installer",
+			},
+			config = function()
+				-- Ok I really don't understand this. If I remove this function
+				-- wrapper, then any local function defined within config/lspconfig
+				-- won't be usable by the setup function. This makes no sense.
+				require("config.lspconfig").setup()
+			end,
+		})
 
-    use({
-      "ruifm/gitlinker.nvim",
-      requires = "nvim-lua/plenary.nvim",
-      keys = {
-        "<leader>gy",
-      },
-      config = function()
-        require("gitlinker").setup()
-      end,
-    })
+		use({
+			"ruifm/gitlinker.nvim",
+			requires = "nvim-lua/plenary.nvim",
+			keys = {
+				"<leader>gy",
+			},
+			config = function()
+				require("gitlinker").setup()
+			end,
+		})
 
-    use({
-      "justinmk/vim-dirvish",
-      requires = {
-        "kristijanhusak/vim-dirvish-git",
-        "roginfarrer/vim-dirvish-dovish",
-      },
-      config = config.dirvish,
-    })
+		use({
+			"justinmk/vim-dirvish",
+			requires = {
+				"kristijanhusak/vim-dirvish-git",
+				"roginfarrer/vim-dirvish-dovish",
+			},
+			config = config.dirvish,
+		})
 
-    use({
-      "segeljakt/vim-silicon",
-      cmd = { "Silicon" },
-    })
+		use({
+			"segeljakt/vim-silicon",
+			cmd = { "Silicon" },
+		})
 
-    use({
-      "nvim-neorg/neorg",
-      config = require("config.neorg").setup,
-      requires = {
-        "nvim-telescope/telescope.nvim",
-        "nvim-lua/plenary.nvim",
-        "nvim-neorg/neorg-telescope",
-      },
-    })
+		use({
+			"nvim-neorg/neorg",
+			config = require("config.neorg").setup,
+			requires = {
+				"nvim-lua/plenary.nvim",
+				"~/dev/neorg-telescope",
+			},
+		})
 
-    if packer_bootstrap then
-      require("packer").sync()
-    end
-  end,
-  config = {
-    compile_path = vim.fn.stdpath("config") .. "/plugin/packer_compiled.lua",
-    display = {
-      open_fn = function()
-        return require("packer.util").float({ border = "rounded" })
-      end,
-    },
-    profile = {
-      enable = true,
-    },
-  },
+		if packer_bootstrap then
+			require("packer").sync()
+		end
+	end,
+	config = {
+		compile_path = vim.fn.stdpath("config") .. "/plugin/packer_compiled.lua",
+		display = {
+			open_fn = function()
+				return require("packer.util").float({ border = "rounded" })
+			end,
+		},
+		profile = {
+			enable = true,
+		},
+	},
 })
