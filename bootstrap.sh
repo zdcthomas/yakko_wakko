2d6e001a87314585e11f0cf4e7f04d34162098fe
zdcthomas <zdcthomas@yahoo.com>
2022-10-31 10:53:30 -0600

    need to manually install homebrew annoyingly

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,10 @@
 # sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume --daemon
 # . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 pushd ~/yakko_wakko/
 
 if [[ "$OSTYPE" =~ "darwin" ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   nix --extra-experimental-features nix-command --extra-experimental-features flakes build .#darwinConfigurations.$(hostname -s).system && ./result/sw/bin/darwin-rebuild switch --flake .
 else
   echo "No known setup for non darwin env, Sorry!"
