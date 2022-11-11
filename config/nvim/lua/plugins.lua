4f8d94aee480f2c4dd2ef144fc9b4ee3e7c88dfe
zdcthomas <zdcthomas@yahoo.com>
2022-11-11 12:35:17 -0700

    trying out neo tree

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -24,6 +24,19 @@ end
 
 return require("packer").startup({
 	function(use)
+		use({
+			"nvim-neo-tree/neo-tree.nvim",
+			branch = "v2.x",
+			requires = {
+				"nvim-lua/plenary.nvim",
+				"kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+				"MunifTanjim/nui.nvim",
+			},
+			config = function()
+				require("config.neo_tree").config()
+			end,
+		})
+
 		use("wbthomason/packer.nvim")
 		use("ronny/birds-of-paradise.vim")
 		use("sainnhe/everforest")
