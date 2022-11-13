88be48e7529d6f3a6c109ffc88e99d49e5759990
zdcthomas <zdcthomas@yahoo.com>
2022-11-12 22:31:15 -0700

    beginning of nixos config

---
+++ nix/nixos_configs/sad_frog.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+      /etc/nixos/configuration.nix
+    ];
+
+  nix = {
+    trustedUsers = [ "root" ];
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  documentation.man.enable = true;
+}
