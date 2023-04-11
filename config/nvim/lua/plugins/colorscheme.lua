76cd32de779fa7f0e2b9fd5d0bc47ebbade27bbd
zdcthomas <zdcthomas@yahoo.com>
2023-04-11 15:06:46 -0400

    moves colorschemes to other file

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,55 @@
+return {
+
+	{
+		"savq/melange-nvim",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			-- vim.cmd.colorscheme("melange")
+		end,
+	},
+	{
+		"sainnhe/everforest",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			vim.g.everforest_diagnostic_virtual_text = "colored"
+			vim.g.everforest_enable_italic = 1
+			vim.cmd.colorscheme("everforest")
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		priority = 1000,
+		-- cmd = "colorscheme",
+		-- lazy = false,
+		config = function()
+			require("kanagawa").setup({
+				compile = true, -- enable compiling the colorscheme
+				undercurl = true, -- enable undercurls
+				commentStyle = { italic = true },
+				functionStyle = {},
+				keywordStyle = { italic = true },
+				statementStyle = { bold = true },
+				typeStyle = {},
+				transparent = false, -- do not set background color
+				dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+				terminalColors = true, -- define vim.g.terminal_color_{0,17}
+				colors = { -- add/modify theme and palette colors
+					palette = {},
+					theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+				},
+				overrides = function(colors) -- add/modify highlights
+					return {}
+				end,
+				theme = "wave", -- Load "wave" theme when 'background' option is not set
+				background = { -- map the value of 'background' option to a theme
+					dark = "wave", -- try "dragon" !
+					light = "lotus",
+				},
+			})
+
+			-- vim.cmd.colorscheme("kanagawa")
+		end,
+	},
+}
