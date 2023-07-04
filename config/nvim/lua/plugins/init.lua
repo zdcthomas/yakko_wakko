965855ee2da0a88cc610b403e445f1593cee48fc
zdcthomas <zdcthomas@yahoo.com>
2023-07-04 17:14:13 -0400

    indent blankline can't handle z{l,h}

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -4,36 +4,6 @@
 
 return {
 	{
-		"lukas-reineke/indent-blankline.nvim",
-		-- lazy = false,
-		-- cmd = "VeryLazy",
-		lazy = false,
-		config = function()
-			require("indent_blankline").setup({
-				-- for example, context is off by default, use this to turn it on
-				-- show_current_context = true,
-				-- show_current_context_start = true,
-				-- buftype_exclude = { "terminal", "nofile", "quickfix", "prompt" },
-				filetype_exclude = {
-					"lspinfo",
-					"packer",
-					"checkhealth",
-					"help",
-					"man",
-					"",
-					"startify",
-					"help",
-					"startify",
-					"dashboard",
-					"packer",
-					"neogitstatus",
-					"NvimTree",
-					"Trouble",
-				},
-			})
-		end,
-	},
-	{
 		"echasnovski/mini.splitjoin",
 		version = false,
 		keys = { {
