0faaea27d06c7acc101da6500a6b9ce5d348a44f
zdcthomas <zdcthomas@yahoo.com>
2023-07-16 23:41:33 -0400

    add autopairs for rust generics

--- config/nvim/lua/plugins/autopairs.lua
+++ config/nvim/lua/plugins/autopairs.lua
@@ -8,6 +8,16 @@ return {
 			disable_in_macro = true,
 		})
 
+		local Rule = require("nvim-autopairs.rule")
+		local npairs = require("nvim-autopairs")
+		local cond = require("nvim-autopairs.conds")
+
+		npairs.add_rules({
+			Rule("<", ">", { "rust" }):with_pair(cond.before_regex("%a+")):with_move(function(opts)
+				return opts.char == ">"
+			end),
+		})
+
 		-- local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 		-- local cmp_status_ok, cmp = pcall(require, "cmp")
 		-- if not cmp_status_ok then
