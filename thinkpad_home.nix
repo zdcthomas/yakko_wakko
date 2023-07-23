42346d432425444ce0f77de9a73b9f53ddb4a45f
zdcthomas <zdcthomas@yahoo.com>
2023-07-23 14:13:21 -0400

    move out bash.nix

--- thinkpad_home.nix
+++ thinkpad_home.nix
@@ -317,41 +317,6 @@ in
       enableFishIntegration = true;
     };
 
-    bash = {
-      enable = true;
-      enableCompletion = true;
-      shellOptions = [
-        "histappend"
-        "checkwinsize"
-        "extglob"
-      ];
-      initExtra = ''
-        bind '"\e[A": history-search-backward'
-        bind '"\e[B": history-search-forward'
-
-        git_branch() {
-            # git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-            git branch 2>/dev/null | grep '^*' | colrm 1 2
-        }
-
-        function dirty(){
-        	if [[ $(git status --porcelain 2> /dev/null)  ]];
-        		then 
-        			echo "*"; 
-        	fi
-        }
-
-        cyan="\[\033[36m\]"
-        white="\[\033[m\]"
-        green="\[\033[32m\]"
-        yellow="\[\033[33;1m\]"
-        in_prompt="| => "
-        export PS1="$cyan\u$white@$yellow \w$white \$(git_branch) \$(dirty) \n$in_prompt"
-        export PS2=$in_prompt
-        export PS2="| ?> "
-      '';
-    };
-
     gh = {
       enable = true;
       settings = {
