6b1be5eb6eb438789a74ad9876f52d52e4a41443
zdcthomas <zdcthomas@yahoo.com>
2023-08-24 21:34:17 -0400

    add zathura config

--- nix/nixos_configs/thinkpad/configuration.nix
+++ nix/nixos_configs/thinkpad/configuration.nix
@@ -1,7 +1,6 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
+#  ----------------------------------------------------------------
+#  |    L580 (type 20LW, 20LX) Laptops (ThinkPad) - Type 20LW |
+#  ----------------------------------------------------------------
 args@{ config, pkgs, overlays, inputs, ... }:
 let
   username = "zdcthomas";
