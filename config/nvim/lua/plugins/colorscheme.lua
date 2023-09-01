d167751502f0a5901d7691baf7ad1ce77cf41425
zdcthomas <zdcthomas@yahoo.com>
2023-09-01 13:49:11 -0400

    add lazy lock

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -139,16 +139,16 @@ return {
 					folds = true,
 				},
 				strikethrough = true,
-				invert_selection = true,
+				invert_selection = false,
 				invert_signs = false,
 				invert_tabline = false,
-				invert_intend_guides = false,
+				invert_intend_guides = true,
 				inverse = true, -- invert background for search, diffs, statuslines and errors
-				contrast = "hard", -- can be "hard", "soft" or empty string
+				contrast = "soft", -- can be "hard", "soft" or empty string
 				palette_overrides = {},
 				overrides = {},
-				dim_inactive = false,
-				transparent_mode = true,
+				dim_inactive = true,
+				transparent_mode = false,
 			})
 		end,
 	},
