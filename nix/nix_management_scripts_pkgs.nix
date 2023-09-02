2d4ce591f2266ada99698271d6373fe4a5464fce
zdcthomas <zdcthomas@yahoo.com>
2023-09-02 12:08:36 -0400

    remove unneeded management scripts

--- nix/nix_management_scripts_pkgs.nix
+++ nix/nix_management_scripts_pkgs.nix
@@ -1,29 +1,10 @@
 { pkgs, homeDirectory }:
 
 [
-  (
-    pkgs.writeScriptBin "dot-switch" ''
-      nix build --no-link ${homeDirectory}/yakko_wakko#homeConfigurations.$USER.activationPackage
-      "$(nix path-info ${homeDirectory}/yakko_wakko#homeConfigurations.$USER.activationPackage)"/activate
-    ''
-  )
 
   (
     pkgs.writeScriptBin "dar-switch" ''
       nix build ${homeDirectory}/yakko_wakko#darwinConfigurations.$(hostname -s).system && ${homeDirectory}/yakko_wakko/result/sw/bin/darwin-rebuild switch --flake ${homeDirectory}/yakko_wakko
-    ''
-  )
-
-
-  (
-    pkgs.writeScriptBin "dot-update" ''
-      nix flake update
-    ''
-  )
-
-  (
-    pkgs.writeScriptBin "dot-edit" ''
-      nvim ${homeDirectory}/yakko_wakko/home.nix
     ''
   )
 
