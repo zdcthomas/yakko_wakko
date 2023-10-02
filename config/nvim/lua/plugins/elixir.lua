18ae77cc80e457f87fad183f3b471900dc4bec0f
zdcthomas <zdcthomas@yahoo.com>
2023-10-02 13:09:56 -0400

    add elixir tools

---
+++ config/nvim/lua/plugins/elixir.lua
@@ -0,0 +1,34 @@
+return {
+	-- {
+	-- 	"elixir-tools/elixir-tools.nvim",
+	-- 	version = "*",
+	-- 	event = { "BufReadPre", "BufNewFile" },
+	-- 	config = function()
+	-- 		local elixir = require("elixir")
+	-- 		local elixirls = require("elixir.elixirls")
+	--
+	-- 		local common_on_attach = require("plugins.lspconfig.shared").common_on_attach
+	--
+	-- 		elixir.setup({
+	-- 			nextls = { enable = true },
+	-- 			credo = {},
+	-- 			elixirls = {
+	-- 				enable = true,
+	-- 				settings = elixirls.settings({
+	-- 					dialyzerEnabled = false,
+	-- 					enableTestLenses = false,
+	-- 				}),
+	-- 				on_attach = function(client, bufnr)
+	-- 					vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+	-- 					vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+	-- 					vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+	-- 					common_on_attach(client, bufnr)
+	-- 				end,
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 	},
+	-- },
+}
