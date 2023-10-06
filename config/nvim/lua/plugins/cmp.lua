9ab0d92470421b744f9ae12426a9ce9cddc1b95e
zdcthomas <zdcthomas@yahoo.com>
2023-10-06 14:56:07 -0400

    update crates.nvim config

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -18,118 +18,57 @@ local feedkey = function(key, mode)
 	vim.api.nvim_feedkeys(replace_termcodes(key), mode, true)
 end
 
-local function mappings()
-	local luasnip = require("luasnip")
-	local cmp = require("cmp")
-	return {
-		["<C-n>"] = cmp.mapping(function(fallback)
-			if luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump(1)
-			else
-				cmp.complete()
-			end
-		end, {
-			"i",
-			"s",
-		}),
-		["<C-p>"] = cmp.mapping(function(fallback)
-			if luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump(-1)
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}),
-		["<C-j>"] = cmp.mapping(function(fallback)
-			if cmp.visible() and cmp.get_selected_entry() then
-				cmp.scroll_docs(4)
-			elseif luasnip.choice_active() then
-				luasnip.change_choice(1)
-			else
-				fallback()
-			end
-		end, { "i", "c" }),
-		["<C-k>"] = cmp.mapping(function(fallback)
-			if cmp.visible() and cmp.get_selected_entry() then
-				cmp.scroll_docs(-4)
-			elseif luasnip.choice_active() then
-				luasnip.change_choice(-1)
-			else
-				fallback()
-			end
-		end, { "i", "c" }),
-		["<C-c>"] = cmp.mapping(cmp.mapping.close(), { "i", "c" }),
-		["<CR>"] = cmp.mapping({
-			i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = false }),
-		}),
-		["<Tab>"] = cmp.mapping({
-			c = function(_)
-				if cmp.visible() then
-					cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-				else
-					if vim.fn.getcmdtype() == "/" then
-						cmp.complete()
-					else
-						feedkey("<Tab>", "tn")
-					end
-				end
-			end,
-			i = function(fallback)
-				if cmp.visible() then
-					cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-				-- elseif has_words_before() then
-				-- 	vim.fn.feedkeys(replace_termcodes("<Tab>"), "n")
-				else
-					fallback()
-				end
-			end,
-		}),
-		["<S-Tab>"] = cmp.mapping({
-			c = function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-				else
-					if vim.fn.getcmdtype() == "/" then
-						cmp.complete()
-					else
-						fallback()
-					end
-				end
-			end,
-			i = function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-				-- elseif has_words_before() then
-				-- 	vim.fn.feedkeys(replace_termcodes("<S-Tab>"), "n")
-				else
-					fallback()
-				end
-			end,
-		}),
-	}
-end
-
 return {
 	{
 		"saecki/crates.nvim",
+		version = false,
+		event = { "BufRead Cargo.toml" },
+		dependencies = { "nvim-lua/plenary.nvim" },
 		init = function()
 			vim.api.nvim_create_autocmd("BufRead", {
 				group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
 				pattern = "Cargo.toml",
-				callback = function()
-					local cmp = require("cmp")
+				callback = function(ev)
 					---@diagnostic disable-next-line: missing-fields
-					cmp.setup.buffer({ sources = { { name = "crates" } } })
+					local crates = require("crates")
+					local opts = { silent = true, buffer = ev.buf }
+
+					vim.keymap.set("n", "<leader>ct", crates.toggle, opts)
+					vim.keymap.set("n", "<leader>cr", crates.reload, opts)
+
+					vim.keymap.set("n", "<leader>cv", crates.show_versions_popup, opts)
+					vim.keymap.set("n", "<leader>cf", crates.show_features_popup, opts)
+					vim.keymap.set("n", "<leader>cd", crates.show_dependencies_popup, opts)
+
+					vim.keymap.set("n", "<leader>cu", crates.update_crate, opts)
+					vim.keymap.set("v", "<leader>cu", crates.update_crates, opts)
+					vim.keymap.set("n", "<leader>ca", crates.update_all_crates, opts)
+					vim.keymap.set("n", "<leader>cU", crates.upgrade_crate, opts)
+					vim.keymap.set("v", "<leader>cU", crates.upgrade_crates, opts)
+					vim.keymap.set("n", "<leader>cA", crates.upgrade_all_crates, opts)
+
+					vim.keymap.set("n", "<leader>ce", crates.expand_plain_crate_to_inline_table, opts)
+					vim.keymap.set("n", "<leader>cE", crates.extract_crate_into_table, opts)
+
+					vim.keymap.set("n", "<leader>cH", crates.open_homepage, opts)
+					vim.keymap.set("n", "<leader>cR", crates.open_repository, opts)
+					vim.keymap.set("n", "<leader>cD", crates.open_documentation, opts)
+					vim.keymap.set("n", "<leader>cC", crates.open_crates_io, opts)
+
+					vim.keymap.set("n", "gh", crates.show_popup, opts)
 				end,
 			})
 		end,
-		event = { "BufRead Cargo.toml" },
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("crates").setup()
-		end,
+		opts = {
+			popup = {
+				border = "rounded",
+			},
+			src = {
+				cmp = {
+					enabled = true,
+				},
+			},
+		},
 	},
 	{
 		"hrsh7th/nvim-cmp",
@@ -150,6 +89,7 @@ return {
 		config = function()
 			-- See lspconfig comment on why this is in a function wrapper
 			local cmp = require("cmp")
+			local luasnip = require("luasnip")
 			---@diagnostic disable-next-line: missing-fields
 			cmp.setup({
 				snippet = {
@@ -177,12 +117,100 @@ return {
 				---@diagnostic disable-next-line: missing-fields
 				matching = { disallow_fuzzy_matching = false },
 				preselect = cmp.PreselectMode.None,
-				mapping = mappings(),
+				mapping = {
+					["<C-n>"] = cmp.mapping(function(fallback)
+						if luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump(1)
+						else
+							cmp.complete()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+					["<C-p>"] = cmp.mapping(function(fallback)
+						if luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump(-1)
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+					["<C-j>"] = cmp.mapping(function(fallback)
+						if cmp.visible() and cmp.get_selected_entry() then
+							cmp.scroll_docs(4)
+						elseif luasnip.choice_active() then
+							luasnip.change_choice(1)
+						else
+							fallback()
+						end
+					end, { "i", "c" }),
+					["<C-k>"] = cmp.mapping(function(fallback)
+						if cmp.visible() and cmp.get_selected_entry() then
+							cmp.scroll_docs(-4)
+						elseif luasnip.choice_active() then
+							luasnip.change_choice(-1)
+						else
+							fallback()
+						end
+					end, { "i", "c" }),
+					["<C-c>"] = cmp.mapping(cmp.mapping.close(), { "i", "c" }),
+					["<CR>"] = cmp.mapping({
+						i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = false }),
+					}),
+					["<Tab>"] = cmp.mapping({
+						c = function(_)
+							if cmp.visible() then
+								cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+							else
+								if vim.fn.getcmdtype() == "/" then
+									cmp.complete()
+								else
+									feedkey("<Tab>", "tn")
+								end
+							end
+						end,
+						i = function(fallback)
+							if cmp.visible() then
+								cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+							-- elseif has_words_before() then
+							-- 	vim.fn.feedkeys(replace_termcodes("<Tab>"), "n")
+							else
+								fallback()
+							end
+						end,
+					}),
+					["<S-Tab>"] = cmp.mapping({
+						c = function(fallback)
+							if cmp.visible() then
+								cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+							else
+								if vim.fn.getcmdtype() == "/" then
+									cmp.complete()
+								else
+									fallback()
+								end
+							end
+						end,
+						i = function(fallback)
+							if cmp.visible() then
+								cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+							-- elseif has_words_before() then
+							-- 	vim.fn.feedkeys(replace_termcodes("<S-Tab>"), "n")
+							else
+								fallback()
+							end
+						end,
+					}),
+				},
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp", priority = 9 },
 					{ name = "luasnip", priority = 10 },
 					{ name = "buffer", priority = 7, keyword_length = 3 },
 					{ name = "path", priority = 5 },
+					{ name = "crates" },
 				}, {
 					{ name = "calc" },
 				}),
