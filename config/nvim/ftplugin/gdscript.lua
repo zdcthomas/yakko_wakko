466907c088673b870c36904b90dd12fa66ff82e3
zdcthomas <zdcthomas@yahoo.com>
2023-10-24 10:50:31 -0400

    fix gdscript config

--- config/nvim/ftplugin/gdscript.lua
+++ config/nvim/ftplugin/gdscript.lua
@@ -1,7 +1,3 @@
-local pipe = "/tmp/nvim.pipe"
-vim.api.nvim_command('echo serverstart("' .. pipe .. '")')
-
--- set softtabstop=0 noexpandtab
 vim.bo.softtabstop = 0
 vim.bo.expandtab = false
 vim.bo.shiftwidth = 4
