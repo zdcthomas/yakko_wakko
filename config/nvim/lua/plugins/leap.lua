c1fa270c4c0222247e9d59dd16f8d03f345bd78f
zdcthomas <zdcthomas@yahoo.com>
2023-10-24 01:10:51 -0400

    switch to leap.nvim to test out new motions

--- config/nvim/lua/plugins/leap.lua
+++ config/nvim/lua/plugins/leap.lua
@@ -1,9 +1,44 @@
 return {
 	{
-		"ggandor/leap.nvim",
-		lazy = false,
-		config = function()
-			require("leap").add_default_mappings()
-		end,
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {
+			modes = {
+				search = {
+					enabled = false,
+				},
+				char = {
+					enabled = false,
+					keys = {},
+				},
+			},
+		},
+
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"<c-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash Search",
+			},
+		},
 	},
 }
