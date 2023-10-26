8c023accbcf7d28a092cd50d29e2173e7f2fab0c
zdcthomas <zdcthomas@yahoo.com>
2023-10-26 15:05:29 -0400

    Update notify, dressing, and rename leap plugin file

--- config/nvim/lua/plugins/dressing.lua
+++ config/nvim/lua/plugins/dressing.lua
@@ -1,21 +1,31 @@
 return {
 	"stevearc/dressing.nvim",
-	event = "BufReadPost",
-	config = function()
-		require("dressing").setup({
-			input = {
-				enabled = true,
-				default_prompt = "➤ ",
-				insert_only = false,
-
-				-- These are passed to nvim_open_win
-				relative = "cursor",
-				border = "rounded",
-			},
-			select = {
-				enabled = true,
-				backend = { "builtin", "telescope", "nui" },
-			},
-		})
+	-- event = "BufReadPost",
+	init = function()
+		---@diagnostic disable-next-line: duplicate-set-field
+		vim.ui.select = function(...)
+			require("lazy").load({ plugins = { "dressing.nvim" } })
+			return vim.ui.select(...)
+		end
+		---@diagnostic disable-next-line: duplicate-set-field
+		vim.ui.input = function(...)
+			require("lazy").load({ plugins = { "dressing.nvim" } })
+			return vim.ui.input(...)
+		end
 	end,
+	opts = {
+		input = {
+			enabled = true,
+			default_prompt = "➤ ",
+			insert_only = false,
+
+			-- These are passed to nvim_open_win
+			relative = "cursor",
+			border = "rounded",
+		},
+		select = {
+			enabled = true,
+			backend = { "builtin", "telescope", "nui" },
+		},
+	},
 }
--- config/nvim/lua/plugins/notify.lua
+++ config/nvim/lua/plugins/notify.lua
@@ -22,6 +22,15 @@ return {
 			DEBUG = "",
 			TRACE = "✎",
 		},
+		max_height = function()
+			return math.floor(vim.o.lines * 0.75)
+		end,
+		max_width = function()
+			return math.floor(vim.o.columns * 0.75)
+		end,
+		on_open = function(win)
+			vim.api.nvim_win_set_config(win, { zindex = 100 })
+		end,
 	},
 	init = function()
 		vim.notify = require("notify")
