c3595e6ee2a08b6de678e7178cd1d84a1e5c9cab
zdcthomas <zdcthomas@yahoo.com>
2023-11-15 15:55:50 -0500

    move dap stuff around

--- config/nvim/lua/plugins/dap-ui.lua
+++ config/nvim/lua/plugins/dap-ui.lua
@@ -1,9 +1,6 @@
 return {
 	{
 		"rcarriga/nvim-dap-ui",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-		},
 		opts = {
 			layouts = {
 				{
--- config/nvim/lua/plugins/dap.lua
+++ config/nvim/lua/plugins/dap.lua
@@ -11,156 +11,11 @@ end
 
 return {
 	{
-		"rcarriga/nvim-dap-ui",
-		opts = {
-			layouts = {
-				{
-					elements = {
-						{
-							id = "console",
-							size = 0.1,
-						},
-						{
-							id = "breakpoints",
-							size = 0.1,
-						},
-						{
-							id = "stacks",
-							size = 0.1,
-						},
-						{
-							id = "watches",
-							size = 0.1,
-						},
-						{
-							id = "scopes",
-							size = 0.5,
-						},
-					},
-					position = "left",
-					size = 40,
-				},
-				{
-					elements = {
-						{
-							id = "repl",
-							size = 1,
-						},
-					},
-					position = "bottom",
-					size = 10,
-				},
-			},
-		},
-		keys = {
-			{
-				"<leader>du",
-				function()
-					require("dapui").toggle({})
-				end,
-				desc = "Dap UI",
-			},
-			{
-				"<leader>de",
-				function()
-					require("dapui").eval()
-				end,
-				desc = "Eval",
-				mode = { "n", "v" },
-			},
-		},
-		config = function(_, opts)
-			local dap, dapui = require("dap"), require("dapui")
-			dapui.setup(opts)
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				dapui.close()
-			end
-		end,
-	},
-	{
-		"theHamsta/nvim-dap-virtual-text",
-		opts = {
-			enabled = true, -- enable this plugin (the default)
-			enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-			highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-			highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-			show_stop_reason = true, -- show stop reason when stopped for exceptions
-			commented = false, -- prefix virtual text with comment string
-			only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
-			all_references = false, -- show virtual text on all all references of the variable (not only definitions)
-			clear_on_continue = false, -- clear virtual text on "continue" (might cause flickering when stepping)
-			--- A callback that determines how a variable is displayed or whether it should be omitted
-			--- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
-			--- @param buf number
-			--- @param stackframe dap.StackFrame https://microsoft.github.io/debug-adapter-protocol/specification#Types_StackFrame
-			--- @param node userdata tree-sitter node identified as variable definition of reference (see `:h tsnode`)
-			--- @param options nvim_dap_virtual_text_options Current options for nvim-dap-virtual-text
-			--- @return string|nil A text how the virtual text should be displayed or nil, if this variable shouldn't be displayed
-			display_callback = function(variable, buf, stackframe, node, options)
-				if options.virt_text_pos == "inline" then
-					return " = " .. variable.value
-				else
-					return variable.name .. " = " .. variable.value
-				end
-			end,
-			-- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text. Use 'eol' to set to end of line
-			virt_text_pos = vim.fn.has("nvim-0.10") == 1 and "inline" or "eol",
-
-			-- experimental features:
-			all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-			virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
-			virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
-			-- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
-		},
-	},
-	{
 		"mfussenegger/nvim-dap",
 		dependencies = {
-
-			{
-				"jbyuki/one-small-step-for-vimkind",
-				config = function()
-					local dap = require("dap")
-					dap.adapters.nlua = function(callback, conf)
-						local adapter = {
-							type = "server",
-							host = conf.host or "127.0.0.1",
-							port = conf.port or 8086,
-						}
-						if conf.start_neovim then
-							local dap_run = dap.run
-							dap.run = function(c)
-								adapter.port = c.port
-								adapter.host = c.host
-							end
-							require("osv").run_this()
-							dap.run = dap_run
-						end
-						callback(adapter)
-					end
-					dap.configurations.lua = {
-						{
-							type = "nlua",
-							request = "attach",
-							name = "Run this file",
-							start_neovim = {},
-						},
-						{
-							type = "nlua",
-							request = "attach",
-							name = "Attach to running Neovim instance (port = 8086)",
-							port = 8086,
-						},
-					}
-				end,
-			},
+			"jbyuki/one-small-step-for-vimkind",
 			"theHamsta/nvim-dap-virtual-text",
+			"rcarriga/nvim-dap-ui",
 			{
 				"nvim-telescope/telescope-dap.nvim",
 				dependencies = {
@@ -175,10 +30,120 @@ return {
 				dependencies = {
 					"hrsh7th/nvim-cmp",
 				},
-			},
+				config = function()
+					require("cmp").setup({
+						enabled = function()
+							return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt"
+								or require("cmp_dap").is_dap_buffer()
+						end,
+					})
 
-			"rcarriga/nvim-dap-ui",
+					require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+						sources = {
+							{ name = "dap" },
+						},
+					})
+				end,
+			},
 		},
+		init = function()
+			local hint = [[
+		        ^ ^Step^ ^ ^      ^ ^     Action
+		    ----^-^-^-^--^-^----  ^-^-------------------
+		        ^ ^back^ ^ ^     ^_t_: toggle breakpoint
+		        ^ ^ _K_^ ^        _T_: clear breakpoints
+		    out _H_ ^ ^ _L_ into  _c_: continue
+		        ^ ^ _J_ ^ ^       _x_: terminate
+		        ^ ^over ^ ^     ^^_r_: open repl
+
+		        ^ ^  _q_: exit
+		    ]]
+
+			local hydra = {
+				name = "Debug",
+				hint = hint,
+				config = {
+					color = "pink",
+					invoke_on_body = true,
+					hint = {
+						type = "window",
+						border = "rounded",
+						position = "top-right",
+					},
+				},
+				mode = { "n" },
+				body = "<leader>d,",
+				heads = {
+					{
+						"H",
+						function()
+							require("dap").step_out()
+						end,
+						{ desc = "step out" },
+					},
+					{
+						"J",
+						function()
+							require("dap").step_over()
+						end,
+						{ desc = "step over" },
+					},
+					{
+						"K",
+						function()
+							require("dap").step_back()
+						end,
+						{ desc = "step back" },
+					},
+					{
+						"L",
+						function()
+							require("dap").step_into()
+						end,
+						{ desc = "step into" },
+					},
+					{
+						"t",
+						function()
+							require("dap").toggle_breakpoint()
+						end,
+						{ desc = "toggle breakpoint" },
+					},
+					{
+						"T",
+						function()
+							require("dap").clear_breakpoints()
+						end,
+						{ desc = "clear breakpoints" },
+					},
+					{
+						"c",
+						function()
+							require("dap").continue()
+						end,
+						{ desc = "continue" },
+					},
+					{
+						"x",
+						function()
+							require("dap").terminate()
+						end,
+						{ desc = "terminate" },
+					},
+					{
+						"r",
+						function()
+							require("dap").repl.open()
+						end,
+						{ exit = true, desc = "open repl" },
+					},
+					{ "q", nil, { exit = true, nowait = true, desc = "exit" } },
+				},
+			}
+
+			local glob_hyd = require("plugins.hydra.global_hydra")
+			glob_hyd.add_g_hydra({ key = "d", hydra = hydra, desc = "Debugger" })
+		end,
 		keys = {
 			{
 				"<leader>dB",
@@ -243,13 +208,13 @@ return {
 				end,
 				desc = "Up",
 			},
-			-- {
-			-- 	"<leader>dl",
-			-- 	function()
-			-- 		require("dap").run_last()
-			-- 	end,
-			-- 	desc = "Run Last",
-			-- },
+			{
+				"<leader>dl",
+				function()
+					require("dap").run_last()
+				end,
+				desc = "Run Last",
+			},
 			{
 				"<leader>do",
 				function()
@@ -258,7 +223,7 @@ return {
 				desc = "Step Out",
 			},
 			{
-				"<leader>dl",
+				"<leader>dO",
 				function()
 					require("dap").step_over()
 				end,
@@ -301,6 +266,10 @@ return {
 			},
 		},
 		config = function()
+			-- TODO: <14-11-23, zdcthomas> when I'm not busy, make this lua
+			vim.cmd([[
+        autocmd FileType dap-float nnoremap <buffer><silent> q <cmd>close!<CR>
+      ]])
 			local dap = require("dap")
 			dap.adapters.godot = {
 				type = "server",
