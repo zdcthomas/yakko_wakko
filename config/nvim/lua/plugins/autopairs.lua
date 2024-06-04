e484ae405585d80d8698e2c88cbadaf310793b06
zdcthomas <zdcthomas@yahoo.com>
2024-06-04 15:02:23 -0400

    update autopairs

--- config/nvim/lua/plugins/autopairs.lua
+++ config/nvim/lua/plugins/autopairs.lua
@@ -1,30 +1,39 @@
 return {
-	"windwp/nvim-autopairs",
-	dependencies = {
-		"hrsh7th/nvim-cmp",
+	{
+		"Ultimate-autopair.nvim",
 	},
-	-- event = "InsertEnter",
-	config = function()
-		local autopairs = require("nvim-autopairs")
-		autopairs.setup({
-			fast_wrap = {},
-			check_ts = true,
-			disable_in_macro = true,
-		})
+	{
+		"windwp/nvim-autopairs",
 
-		local Rule = require("nvim-autopairs.rule")
-		local cond = require("nvim-autopairs.conds")
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/nvim-cmp",
+		},
 
-		autopairs.add_rules({
-			Rule("<", ">", { "rust" }):with_pair(cond.before_regex("%a+")):with_move(function(opts)
-				return opts.char == ">"
-			end),
-		})
+		-- event = "InsertEnter",
+		config = function()
+			local autopairs = require("nvim-autopairs")
+			autopairs.setup({
+				fast_wrap = {},
+				check_ts = true,
+				disable_in_macro = true,
+			})
 
-		-- autopairs.get_rule('"')[1]:with_pair(cond.not_before_regex("(")):with_pair(cond.not_filetypes({"l"}))
+			local Rule = require("nvim-autopairs.rule")
+			local cond = require("nvim-autopairs.conds")
 
-		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-		local cmp = require("cmp")
-		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-	end,
+			autopairs.get_rules("{")[1] = nil
+			autopairs.add_rules({
+				Rule("<", ">", { "rust" }):with_pair(cond.before_regex("%a+")):with_move(function(opts)
+					return opts.char == ">"
+				end),
+			})
+
+			-- autopairs.get_rule('"')[1]:with_pair(cond.not_before_regex("(")):with_pair(cond.not_filetypes({"l"}))
+
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
 }
