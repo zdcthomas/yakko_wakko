8986a627b4493c3b82ca7e5e18c2597b1e18e434
zdcthomas <zdcthomas@yahoo.com>
2024-06-19 13:54:42 -0400

    move substituters

--- flake.nix
+++ flake.nix
@@ -212,10 +212,18 @@ Every file used from anything in a flake _MUST_ and I repeat, _MUST_ be checked 
         };
     };
   };
-  # nixConfig = {
-  #   extra-trusted-substituters = [
-  #   ];
-  #   extra-trusted-public-keys = [
-  #   ];
-  # };
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org?priority=10"
+      "https://nix-community.cachix.org"
+      "https://anyrun.cachix.org"
+      "https://hyprland.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
 }
