68adcd006b56018af8066285ef1540fa44ad394b
zdcthomas <zdcthomas@yahoo.com>
2024-06-22 00:14:44 -0400

    add gitsigns mapping

--- config/nvim/lua/plugins/git/init.lua
+++ config/nvim/lua/plugins/git/init.lua
@@ -27,6 +27,57 @@ return {
 					changedelete = { text = "~" },
 					untracked = { text = "┆" },
 				},
+				on_attach = function(bufnr)
+					local gitsigns = require("gitsigns")
+
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+
+					-- Navigation
+					map("n", "<leader>gn", function()
+						-- if vim.wo.diff then
+						-- 	vim.cmd.normal({ "]c", bang = true })
+						-- else
+						gitsigns.nav_hunk("next")
+						-- end
+					end)
+
+					map("n", "<leader>gp", function()
+						-- if vim.wo.diff then
+						-- 	vim.cmd.normal({ "[c", bang = true })
+						-- else
+						gitsigns.nav_hunk("prev")
+						-- end
+					end)
+
+					-- Actions
+					map("n", "<leader>ga", gitsigns.stage_hunk)
+					map("n", "<leader>gr", gitsigns.reset_hunk)
+					map("v", "<leader>gs", function()
+						gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end)
+					map("v", "<leader>gr", function()
+						gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end)
+					map("n", "<leader>gA", gitsigns.stage_buffer)
+					map("n", "<leader>gu", gitsigns.undo_stage_hunk)
+					map("n", "<leader>gR", gitsigns.reset_buffer)
+					map("n", "<leader>gp", gitsigns.preview_hunk)
+					map("n", "<leader>gb", function()
+						gitsigns.blame_line({ full = true })
+					end)
+					map("n", "<leader>gtb", gitsigns.toggle_current_line_blame)
+					map("n", "<leader>hd", gitsigns.diffthis)
+					map("n", "<leader>hD", function()
+						gitsigns.diffthis("~")
+					end)
+
+					-- Text object
+					map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+				end,
 				signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 				numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
 				linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
