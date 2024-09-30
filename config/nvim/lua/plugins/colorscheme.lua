56f262c1725865b45eaaf9be00f00955fbc37be8
zdcthomas <zdcthomas@yahoo.com>
2024-09-30 15:52:44 -0400

    add imv mime types to opt and add colorschemes

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -13,9 +13,23 @@ return {
 		},
 	}, -- lazy
 	{
+		"qaptoR-nvim/chocolatier.nvim",
+		priority = 1000,
+		config = true,
+		opts = {},
+		init = function()
+			vim.cmd.colorscheme("chocolatier")
+		end,
+	},
+	{
 		"slugbyte/lackluster.nvim",
 		lazy = false,
 		priority = 1000,
+	},
+	{
+		"yorik1984/newpaper.nvim",
+		priority = 1000,
+		config = true,
 	},
 	{
 		"folke/tokyonight.nvim",
@@ -166,7 +180,7 @@ return {
 		"ellisonleao/gruvbox.nvim",
 		priority = 1000,
 		init = function()
-			vim.cmd.colorscheme("gruvbox")
+			-- vim.cmd.colorscheme("gruvbox")
 		end,
 		opts = {
 			undercurl = true,
--- nix/hosts/opt/home.nix
+++ nix/hosts/opt/home.nix
@@ -16,6 +16,28 @@ in {
     ../../modules/home
     # inputs.phinger-hypr.homeManagerModules.hyprcursor-phinger
   ];
+  xdg = {
+    desktopEntries = {
+      imv-dir = {
+        name = "imv-dir";
+        comment = "Image Viewer";
+        exec = "imv-dir %U";
+        icon = "imv-dir";
+        terminal = false;
+        type = "Application";
+        categories = ["Graphics"];
+      };
+    };
+
+    mimeApps.defaultApplications = {
+      "image/png" = "imv-dir.desktop";
+      "image/jpeg" = "imv-dir.desktop";
+      "image/gif" = "imv-dir.desktop";
+      "image/bmp" = "imv-dir.desktop";
+      "image/tiff" = "imv-dir.desktop";
+      "image/x-icon" = "imv-dir.desktop";
+    };
+  };
 
   # colorScheme = inputs.nix-colors.colorSchemes.everforest;
   # colorScheme = inputs.nix-colors.colorSchemes.catppuccin-mocha;
