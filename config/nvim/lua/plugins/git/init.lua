b3a63fa4162ec199f0043df278198ecee3c5a027
zdcthomas <zdcthomas@yahoo.com>
2024-10-09 13:39:52 -0400

    remove fugitive and update lazy lock

--- config/nvim/lua/plugins/git/init.lua
+++ config/nvim/lua/plugins/git/init.lua
@@ -19,14 +19,14 @@ return {
 		},
 		config = function()
 			require("gitsigns").setup({
-				signs = {
-					add = { text = "┃" },
-					change = { text = "┃" },
-					delete = { text = "_" },
-					topdelete = { text = "‾" },
-					changedelete = { text = "~" },
-					untracked = { text = "┆" },
-				},
+				-- signs = {
+				-- 	add = { text = "┃" },
+				-- 	change = { text = "┃" },
+				-- 	delete = { text = "_" },
+				-- 	topdelete = { text = "‾" },
+				-- 	changedelete = { text = "~" },
+				-- 	untracked = { text = "┆" },
+				-- },
 				on_attach = function(bufnr)
 					local gitsigns = require("gitsigns")
 
@@ -85,22 +85,14 @@ return {
 				numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
 				linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
 				word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-				watch_gitdir = {
-					follow_files = true,
-				},
+				-- watch_gitdir = {
+				-- 	follow_files = true,
+				-- },
 				auto_attach = true,
 				attach_to_untracked = true,
 				current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-				current_line_blame_opts = {
-					virt_text = true,
-					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-					delay = 1000,
-					ignore_whitespace = false,
-					virt_text_priority = 100,
-				},
-				current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
 				sign_priority = 6,
-				update_debounce = 100,
+				-- update_debounce = 100,
 				status_formatter = nil, -- Use default
 				max_file_length = 40000, -- Disable if file is longer than this (in lines)
 				preview_config = {
