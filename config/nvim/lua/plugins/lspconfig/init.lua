034de92c189079d3bc1e8e0287bb61cf24a12cd3
zdcthomas <zdcthomas@yahoo.com>
2024-12-22 11:54:05 -0500

    modernize glance stuff

--- config/nvim/lua/plugins/lspconfig/init.lua
+++ config/nvim/lua/plugins/lspconfig/init.lua
@@ -148,18 +148,12 @@ return {
 	{
 		"dnlhc/glance.nvim",
 		cmd = "Glance",
-		keys = {
-			{ "gpd", "<cmd>Glance definitions<CR>" },
-			{ "gpr", "<cmd>Glance references<CR>" },
-			{ "gpt", "<cmd>Glance type_definitions<CR>" },
-			{ "gpi", "<cmd>Glance implementations<CR>" },
-		},
 		opts = function()
 			local actions = require("glance").actions
 			return {
-				theme = { -- This feature might not work properly in nvim-0.7.2
-					-- enable = true, -- Will generate colors for the plugin based on your current colorscheme
-					-- mode = "brighten", -- 'brighten'|'darken'|'auto', 'auto' will set mode based on the brightness of your colorscheme
+				theme = {
+					enable = true,
+					mode = "auto",
 				},
 				list = {
 					position = "left", -- Position of the list window 'left'|'right'
@@ -169,40 +163,30 @@ return {
 					return vim.api.nvim_win_get_width(winid) < 100
 				end,
 				folds = {
-					fold_closed = "󰅂", -- 󰅂 
-					fold_open = "󰅀", -- 󰅀 
-					folded = false,
+					folded = true,
+				},
+				winbar = {
+					enable = true,
 				},
 				border = {
 					enable = true, -- Show window borders. Only horizontal borders allowed
-					top_char = "―",
-					bottom_char = "―",
-				},
-				hooks = {
-					---@diagnostic disable-next-line: unused-local
-					-- before_open = function(results, open, jump, method)
-					-- 	local uri = vim.uri_from_bufnr(0)
-					-- 	if #results == 1 then
-					-- 		local target_uri = results[1].uri or results[1].targetUri
-					--
-					-- 		if target_uri == uri then
-					-- 			jump(results[1])
-					-- 		else
-					-- 			open(results)
-					-- 		end
-					-- 	else
-					-- 		open(results)
-					-- 	end
-					-- end,
+					-- top_char = "―",
+					-- bottom_char = "―",
 				},
 				mappings = {
 					list = {
+						["<Tab>"] = actions.next_location,
+						["<S-Tab>"] = actions.previous_location,
 						["<C-u>"] = actions.preview_scroll_win(5),
 						["<C-d>"] = actions.preview_scroll_win(-5),
 						["<C-l>"] = actions.jump_vsplit,
 						["<C-j>"] = actions.jump_split,
+						["<C-q>"] = actions.quickfix,
 						["st"] = actions.jump_tab,
 						["p"] = actions.enter_win("preview"),
+						["l"] = actions.open_fold,
+						["h"] = actions.close_fold,
+						["<Esc>"] = actions.close,
 					},
 					preview = {
 						["q"] = actions.close,
