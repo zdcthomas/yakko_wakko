67210b514fb6b4a444684655e7856b483d1b6809
zdcthomas <zdcthomas@yahoo.com>
2025-01-08 09:27:39 -0500

    zellij updates

--- nix/modules/home/zellij.nix
+++ nix/modules/home/zellij.nix
@@ -16,29 +16,34 @@ in {
     home.sessionVariables = {
       ZELLIJ_AUTO_ATTACH = "true";
     };
-    # home.file = {
-    #   ".config/zellij/" = {
-    #     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/yakko_wakko/config/zellij";
-    #   };
-    # };
-
-    programs.zellij = {
-      enable = true;
-      settings = {
-        theme = "gruvbox-dark";
-        copy_command = "${pkgs.wl-clipboard}/bin/wl-copy";
-
-        default_mode = "locked";
-
-        pane_frames = false;
-        on_force_close = "detach";
-        ui = {
-          pane_frames = {
-            hide_session_name = true;
-            rounded_corners = true;
-          };
+    home = {
+      file = {
+        ".config/zellij/" = {
+          source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/yakko_wakko/config/zellij";
         };
       };
+      packages = with pkgs; [
+        zellij
+      ];
     };
+
+    # programs.zellij = {
+    #   enable = true;
+    #   settings = {
+    #     theme = "gruvbox-dark";
+    #     copy_command = "${pkgs.wl-clipboard}/bin/wl-copy";
+    #
+    #     default_mode = "locked";
+    #
+    #     pane_frames = false;
+    #     on_force_close = "detach";
+    #     ui = {
+    #       pane_frames = {
+    #         hide_session_name = true;
+    #         rounded_corners = true;
+    #       };
+    #     };
+    #   };
+    # };
   };
 }
