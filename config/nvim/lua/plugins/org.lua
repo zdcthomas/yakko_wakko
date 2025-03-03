c0b979157315524d15ce16ea97016f52e383515c
zdcthomas <zdcthomas@yahoo.com>
2025-03-03 17:16:50 -0500

    remove journal plugin

--- config/nvim/lua/plugins/org.lua
+++ config/nvim/lua/plugins/org.lua
@@ -137,13 +137,6 @@ return {
 					},
 				},
 			})
-
-			-- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
-			-- add ~org~ to ignore_install
-			-- require('nvim-treesitter.configs').setup({
-			--   ensure_installed = 'all',
-			--   ignore_install = { 'org' },
-			-- })
 		end,
 	},
 }
