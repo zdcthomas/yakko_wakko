7e0909b3209d31f391dad4206ba9372c65f0c141
zdcthomas <zdcthomas@yahoo.com>
2025-07-13 18:54:28 -0400

    try out snacks since dressing is dep

---
+++ config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,42 @@
+return {
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+			bigfile = { enabled = true },
+			dashboard = { enabled = false },
+			explorer = { enabled = false },
+			indent = { enabled = false },
+			input = { enabled = true },
+			picker = { enabled = true },
+			notifier = { enabled = false },
+			quickfile = { enabled = true },
+			scope = { enabled = false },
+			scroll = { enabled = false },
+			statuscolumn = { enabled = false },
+			words = { enabled = true },
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "VeryLazy",
+				callback = function()
+					-- Setup some globals for debugging (lazy-loaded)
+					_G.dd = function(...)
+						Snacks.debug.inspect(...)
+					end
+					_G.bt = function()
+						Snacks.debug.backtrace()
+					end
+					vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+					-- Create some toggle mappings
+				end,
+			})
+		end,
+	},
+}
