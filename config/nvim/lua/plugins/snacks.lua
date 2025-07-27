94a53bd85d842ba29415a97d2e319889278a267a
zdcthomas <zdcthomas@yahoo.com>
2025-07-27 11:57:11 -0400

    update ts mod structure

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -3,11 +3,7 @@ return {
 		"folke/snacks.nvim",
 		priority = 1000,
 		lazy = false,
-		---@type snacks.Config
 		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
 			bigfile = { enabled = true },
 			dashboard = { enabled = false },
 			explorer = { enabled = false },
