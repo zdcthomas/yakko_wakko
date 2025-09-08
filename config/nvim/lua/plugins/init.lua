f2be36b92180ee141757f9c5a300eb526127a2a5
zdcthomas <zdcthomas@yahoo.com>
2025-09-08 11:01:07 -0400

    remove smooth scool thing

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -34,4 +34,33 @@ return { -- { "danilamihailov/beacon.nvim", event = "VeryLazy" }, -- lazy calls 
 			eval_on_move = true,
 		},
 	},
+	{
+		"terrastruct/d2-vim",
+		ft = { "d2" },
+	},
+	-- bad dog no biscuits, overwrote j and k and bricked me.
+	-- Banned from the mickey mouse club house
+	-- {
+	-- 	"LuxVim/nvim-luxmotion",
+	-- 	lazy = false,
+	-- 	config = function()
+	-- 		require("luxmotion").setup({
+	-- 			cursor = {
+	-- 				duration = 250,
+	-- 				easing = "ease-out",
+	-- 				enabled = true,
+	-- 			},
+	-- 			scroll = {
+	-- 				duration = 400,
+	-- 				easing = "ease-out",
+	-- 				enabled = true,
+	-- 			},
+	-- 			performance = { enabled = false },
+	-- 			keymaps = {
+	-- 				cursor = true,
+	-- 				scroll = true,
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 }
